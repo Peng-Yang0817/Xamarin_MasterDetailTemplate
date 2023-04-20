7086f03fc4489d088185f6a699570663f52776cf
tcher2007 <tcher2007>
2023-04-20 13:30:38 +0800

    解除綁定功能完成

--- MasterDetailTemplate/Views/BindAndEditPage.xaml.cs
+++ MasterDetailTemplate/Views/BindAndEditPage.xaml.cs
@@ -36,14 +36,15 @@ namespace MasterDetailTemplate.Views
         /// </summary>
         protected override async void OnAppearing()
         {
-            clearAllEntry();
-            ClearStackLayoutData();
             if (!server.IsNetworkAvailable())
             {
                 await DisplayAlert("警告", "請先連接至網際網路。", "確定");
             }
             else
             {
+                clearAllEntry();
+                ClearStackLayoutData();
+
                 Appearing_RefreshView.IsEnabled = true;
                 Appearing_RefreshView.IsRefreshing = true;
                 await Task.Delay(300);
@@ -116,9 +117,13 @@ namespace MasterDetailTemplate.Views
             string string_AquariumTypeId = Entry_AquariumTypeId.Text;
 
             if (string_Email == null ||
+                string_Email == "" ||
                 string_Password == null ||
+                string_Password == "" ||
                 string_AquaruimNum == null ||
-                string_AquariumTypeId == null)
+                string_AquaruimNum == "" ||
+                string_AquariumTypeId == null ||
+                string_AquariumTypeId == "")
             {
                 await DisplayAlert("注意", "請填入完整的資訊!", "確定");
             }
@@ -139,6 +144,7 @@ namespace MasterDetailTemplate.Views
                     if (returnMsg.Status)
                     {
                         await DisplayAlert("完成", returnMsg.Message.ToString(), "確定");
+                        OnAppearing();
                     }
                     else
                     {
@@ -150,10 +156,46 @@ namespace MasterDetailTemplate.Views
             Appearing_RefreshView.IsRefreshing = false;
         }
 
+        /// <summary>
+        /// 刪除按鈕被點擊後，確認是否解綁
+        /// </summary>
+        private async void BtnDelete_Clicked(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            // 取的按鈕的代表的魚缸編號，這裡因為找不到其他方法來賦予按鈕資料，所以暫且只有想到這方法
+            var aquariumUnitNum = button.ClassId;
+
+            // 與用戶確認是否要解綁
+            bool answer = await DisplayAlert("確認",
+                                            "您確定要解綁魚缸\n" + aquariumUnitNum + " ?",
+                                            "是", "否");
+            // 是
+            if (answer)
+            {
+                // 向Server發送解綁要求
+                ReturnMsg returnMsg = await SendUnBindRequest(aquariumUnitNum);
+
+                // 若Server不等於null，那代表有回應
+                if (returnMsg != null)
+                {
+                    if (returnMsg.Status)
+                    {
+                        await DisplayAlert("完成", returnMsg.Message.ToString(), "確定");
+                        OnAppearing();
+                    }
+                    else
+                    {
+                        await DisplayAlert("錯誤", returnMsg.Message.ToString(), "確定");
+                    }
+                }
+
+            }
+        }
+
         // ========================================================================== 取得伺服器回傳的綁定結果
 
         /// <summary>
-        /// 向伺服器發送綁定請求
+        /// 向伺服器發送  綁定  請求
         /// </summary>
         public async Task<ReturnMsg> SendBindRequest(string Email, string Password,
                                                 string AquariumNum, string WaterType)
@@ -211,9 +253,8 @@ namespace MasterDetailTemplate.Views
 
 
         /// <summary>
-        /// 向Server取得使用者魚缸資訊
+        /// 向Server取得使用者魚缸資訊，主要是取得該用戶有那些魚缸
         /// </summary>
-        /// <returns></returns>
         public async Task<List<AquaruimSituation>> GetMyAquaruimSituationsData()
         {
             // 獲得Properties當中目前的使用者Auth001Id
@@ -265,6 +306,62 @@ namespace MasterDetailTemplate.Views
         }
 
 
+
+        /// <summary>
+        /// 向伺服器發送  解除綁定  請求
+        /// </summary>
+        public async Task<ReturnMsg> SendUnBindRequest(string AquariumNum)
+        {
+            // 獲得Properties當中目前的使用者Auth001Id
+            string _Auth001Id = app.Properties[Auth001Id].ToString();
+
+            // 準備裝伺服器回應的資料
+            ReturnMsg returnMsg = new ReturnMsg();
+
+            using (var wb = new WebClient())
+            {
+                var dataSendUse = new NameValueCollection();
+
+                string urlSendUse = server.ServerIP + "/MobileService/unBindService";
+
+                string Bearer = "Bearer " + "jpymJUKgpjPp49GbC6onVCBlNYZfIDHfi5hypNrPXh1";
+                wb.Headers.Add("Authorization", Bearer);
+
+                // 準備魚缸編號，用於查詢
+                dataSendUse["Auth001Id"] = _Auth001Id;
+                dataSendUse["AquariumNum"] = AquariumNum;
+
+                // 使用 CancellationTokenSource 取消等待
+                var cts = new CancellationTokenSource();
+
+                // 建立 timeoutTask 來等待 WaitTimeToServerResponese 的時間
+                var timeoutTask = Task.Delay(TimeSpan.FromSeconds(server.AccessTimeOut), cts.Token);
+
+                // 建立 responseTask 來執行伺服器回應
+                var responseTask = wb.UploadValuesTaskAsync(urlSendUse, "POST", dataSendUse);
+
+                // 等待 timeoutTask 與 responseTask 中的其中一個 Task 完成
+                Task completedTask = await Task.WhenAny(timeoutTask, responseTask);
+
+                cts.Cancel();
+
+                if (completedTask == timeoutTask)
+                {
+                    await DisplayAlert("警告", "伺服器回應超時，請稍後再試。", "確定");
+                    return null;
+                }
+
+                // 如果 responseTask 完成，則繼續執行下面的程式碼
+                var responseSendUse = await responseTask;
+
+                string str = Encoding.UTF8.GetString(responseSendUse);
+
+                // 解析JSON string
+                returnMsg = JsonConvert.DeserializeObject<ReturnMsg>(str);
+
+                return returnMsg;
+            }
+        }
 
         // ========================================================================== 設定模板的區塊
 
@@ -352,6 +449,19 @@ namespace MasterDetailTemplate.Views
             };
 
             // 刪除按鈕 (欄2)
+            Button button = new Button
+            {
+                Text = "解綁",
+                ClassId = data.AquariumUnitNum,
+                TextColor = Color.White,
+                FontSize = 16,
+                WidthRequest = 20,
+                HeightRequest = 40,
+                BackgroundColor = Color.FromHex("#d45e5e"),
+                CornerRadius = 10
+            };
+
+            button.Clicked += BtnDelete_Clicked;
             var stackLayout_Delete = new StackLayout
             {
                 Style = (Style)Application.Current.Resources["Table_Content_Style"],
@@ -362,16 +472,7 @@ namespace MasterDetailTemplate.Views
                         VerticalOptions = LayoutOptions.CenterAndExpand,
                         Children=
                         {
-                            new Button {
-                                Text = "刪除",
-                                ClassId = data.AquariumUnitNum,
-                                TextColor = Color.White,
-                                FontSize = 16,
-                                WidthRequest = 20,
-                                HeightRequest = 40,
-                                BackgroundColor = Color.FromHex("#d45e5e"),
-                                CornerRadius = 10
-                            }
+                            button
                         }
                     }
                 }
